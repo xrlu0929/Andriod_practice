f142c164ac3ebb3c23a0276396b472c11db9dcf2
xrlu0929 <xl2474@tc.columbia.edu>
2018-06-22 18:38:03 -0400

    first commit

--- app/src/main/java/com/myappcompany/rob/exampleproject/MainActivity.java
+++ app/src/main/java/com/myappcompany/rob/exampleproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity; // class for AppCompat
 import android.os.Bundle;
 import android.util.Log; // for Log.i()
 import android.view.View; // the class for View type
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +13,8 @@ public class MainActivity extends AppCompatActivity {
          * here the view refers to the button linked to clickFunction
          */
         Log.i("info", "Button Pressed");
-        // view.setBackgroundColor(99);
-        // Log.i("button color", "getColor(99)");
-        // activity
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        Log.i("name", nameEditText.getText().toString());
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
