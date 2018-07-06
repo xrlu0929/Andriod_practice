744fdf652d7a4604786ce86c010878a725c87159
xrlu0929 <xl2474@tc.columbia.edu>
2018-07-05 23:10:22 -0400

    switch picture resources

--- app/src/main/java/com/myappcompany/rob/exampleproject/MainActivity.java
+++ app/src/main/java/com/myappcompany/rob/exampleproject/MainActivity.java
@@ -1,20 +1,38 @@
 package com.myappcompany.rob.exampleproject;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity; // class for AppCompat
 import android.os.Bundle;
 import android.util.Log; // for Log.i()
 import android.view.View; // the class for View type
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction(View view) {
+    public void login(View view) {
         /**
          * here the view refers to the button linked to clickFunction
          */
         Log.i("info", "Button Pressed");
         EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        EditText password = findViewById(R.id.password);
         Log.i("name", nameEditText.getText().toString());
+        Log.i("password", password.getText().toString());
+
+        Toast.makeText(this, "Hi, " + nameEditText.getText().toString() , Toast.LENGTH_SHORT).show();
+    }
+
+    public void changePic(View view){
+        ImageView myImgView = findViewById(R.id.picture1);
+        if(myImgView.getDrawable().getConstantState() == myImgView.getResources().getDrawable(R.drawable.qinzi).getConstantState()){
+            myImgView.setImageResource(R.drawable.schoolnail);
+        } else {
+            myImgView.setImageResource(R.drawable.qinzi);
+        }
+
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
